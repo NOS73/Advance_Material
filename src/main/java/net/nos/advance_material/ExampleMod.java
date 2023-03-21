e5a1cb85d49459342bc573a9b00107eeb3a22f13
NOS73 <elson2023hk@gmail.com>
2023-03-21 20:13:13 +0000

    First commit

---
+++ src/main/java/net/nos/advance_material/ExampleMod.java
@@ -0,0 +1,42 @@
+package net.nos.advance_material;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.nos.advance_material.item.Moditems;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Advance_Material.MOD_ID)
+public class Advance_Material {
+    public static final String MOD_ID = "advance_material";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Advance_Material() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+
+        Moditems.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void commonSetup(final FMLCommonSetupEvent event)   {
+
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
